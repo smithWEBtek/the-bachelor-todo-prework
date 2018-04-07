04e717be75669e9cb9eebdad8f17a11c0a96f85d
smithwebtek <brad@smithwebtek.com>
2018-04-07 02:06:38 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,53 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+	winner = ''
+	data[season].select do |contestant|
+		if contestant["status"] == "Winner"
+			winner = contestant["name"]
+		end
+	end
+	winner.split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+	data.each do |season, contestants|
+		contestants.each do |contestant|
+			if contestant['occupation'] === occupation
+				return contestant['name']
+			end
+		end
+	end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+	hometown_count = 0
+	data.each do |season, contestants|
+		contestants.each do |contestant|
+			if contestant['hometown'] === hometown
+				hometown_count += 1
+			end
+		end
+	end
+	hometown_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+	data.each do |season, contestants|
+		contestants.each do |contestant|
+			if contestant['hometown'] === hometown
+				return contestant['occupation']
+			end
+		end
+	end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+	age_total = 0
+	contestant_count = 0
+	data[season].each do |contestant|
+		age_total += (contestant['age'].to_f)
+		contestant_count += 1
+	end
+	avg_age = (age_total / contestant_count).round
 end
